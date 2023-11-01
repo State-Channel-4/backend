a46f86b6a6c19df2606ed58859726a908b0a09a0
Nico Serrano <nnicosp@hotmail.com>
2023-11-01 00:02:53 -0500

    auth(fix): delete unused code

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,8 +1,4 @@
-import { Request, Response, NextFunction } from 'express';
-import ethers from 'ethers';
-import ABI from '../abi/channel4.json';
 import jwt from 'jsonwebtoken';
-//import { expressjwt as express_jwt } from 'express-jwt';
 import { expressjwt } from 'express-jwt';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -11,7 +7,6 @@ dotenv.config();
 const jwt_secret = process.env.JWT_SECRET as string;
 
 export const authenticate = expressjwt({ secret: jwt_secret, algorithms: ["HS256"] });
-console.log("authenticate", authenticate);
 
 export const generateToken = (user: { _id: string }) => {
     const token = jwt.sign({ id: user._id }, jwt_secret, { expiresIn: "1d" });
